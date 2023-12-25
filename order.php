f79f601052be281c15c8d651317feb1fdc9537a8
egorpisch23 <135999238+egorpisch23@users.noreply.github.com>
2023-12-25 05:30:52 +0300

    Add files via upload

--- order.php
+++ order.php
@@ -13,6 +13,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap" rel="stylesheet">
 
     <!-- CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -35,7 +38,7 @@
             <div class="cart-section p-5 d-flex justify-content-end">
                 <div class="cart-container position-relative">
                     <a href="order" class="cart-icon position-absolute">
-                        <img src="basket.ico" alt="Cart" style="width: 64px; height: 64px;">
+                        <img src="basket.ico" alt="Cart">
                         <span class="badge bg-danger position-absolute">0</span>
                     </a>
                 </div>
